23f26fe0792df24a21d0de2859eee999aa26be44
codemon99 <spectre1c@gmail.com>
2023-09-09 12:28:15 +0900

    단어 상자 만들기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
 const HomePage = () => {
   return(
-    <div className=" bg-red-300">
-      <div>Dino__kit Wordle</div>
-      <div>
-        <div className=" flex justify-between">
-          <div>a1</div>
-          <div>a2</div>
-          <div>a3</div>
-          <div>a4</div>
-          <div>a5</div>
-        </div>
-        <div className=" flex justify-between">
-          <div>b1</div>
-          <div>b2</div>
-          <div>b3</div>
-          <div>b4</div>
-          <div>b5</div>
-        </div>
+    <div className=" flex justify-center flex-col items-center p-2 ">
+      <div className=" mb-8">Dino__kit Wordle</div>
+      <div className=" pt-5 bg-slate-300 w-1/4 border-solid border-2 border-black rounded-md flex justify-between">
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+      </div>
+      <div className=" pt-5 bg-slate-300 w-1/4 border-solid border-2 border-black rounded-md flex justify-between">
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
       </div>
     </div>
   )
