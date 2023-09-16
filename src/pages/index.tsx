0c9b8a24fc56e2e1d81607ca8d8784013d686e8e
codemon99 <spectre1c@gmail.com>
2023-09-16 12:08:35 +0900

    정답 확인하기 1.0
    입력값이 문제와 같음을 확인한다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,36 @@
+import { useState } from "react";
+
 const HomePage = () => {
+  const [quiz, setQuiz] = useState<stirng>("apple");
+  const [answer, setAnswer] = useState<string>(null);
+
+  const handleForAnwser = (e) => {
+    setAnswer(e.target.value);
+  }
+
+  const validEnterEvent = (e) => {
+    if(e.key === "Enter") handleForClick();
+  }
+
+  const handleForClick = () => {
+    
+    console.log("==========================================================");
+    const quizArray = splitString(quiz);
+    const answerArray = splitString(answer);
+
+    for(let i = 0; i < answerArray.length; i++) {
+      const aData = quizArray.filter((data, index) => i === index && answerArray[i] === data);
+      console.log(aData);
+    }
+    
+    console.log("==========================================================");
+  }
+
+  const splitString = (inputText: string) => {
+    return inputText.split('');
+  }
+
+
   return(
     <div className=" flex justify-center flex-col items-center p-2 ">
       <div className=" mb-8">Dino__kit Wordle</div>
@@ -15,6 +47,10 @@ const HomePage = () => {
           <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
           <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
           <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+      </div>
+      <div className=" bg-orange-200 h-full w-full">
+        <input type="text" onChange={handleForAnwser} onKeyDown={validEnterEvent}></input>
+        <button onClick={handleForClick}>확인</button>
       </div>
     </div>
   )
