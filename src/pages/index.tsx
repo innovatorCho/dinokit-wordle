6b28513167a1b06dd945ba71d6b4e46e11beb822
codemon99 <spectre1c@gmail.com>
2023-09-23 12:10:19 +0900

    퀴즈 정답 확인 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 const HomePage = () => {
   const [quiz, setQuiz] = useState<stirng>("apple");
   const [answer, setAnswer] = useState<string>(null);
+  const [findItem, setFindItem] = useState<string[]>([]);
+  const [quizArr, setQuizArr] = useState<string[]>([]);
+  let itemData:string[] = ['a','b'];
 
   const handleForAnwser = (e) => {
     setAnswer(e.target.value);
@@ -13,17 +16,37 @@ const HomePage = () => {
   }
 
   const handleForClick = () => {
-    
-    console.log("==========================================================");
-    const quizArray = splitString(quiz);
-    const answerArray = splitString(answer);
+    try{
 
-    for(let i = 0; i < answerArray.length; i++) {
-      const aData = quizArray.filter((data, index) => i === index && answerArray[i] === data);
-      console.log(aData);
+      console.log("==========================================================");
+      const quizArray = splitString(quiz);
+      const answerArray = splitString(answer);
+
+      setQuizArr(quizArray);
+
+      for(let i = 0; i < answerArray.length; i++) {
+        const aData = quizArray.filter((data, index) => i === index && answerArray[i] === data);
+        if(aData.length > 0) {
+          console.log(`### task : ${aData} , len ${aData.length}`);
+        } else {
+          console.log(`## failed item ${answerArray[i]}`);
+        }        
+      }
+
+      itemData  = answerArray.map((item, index) => {
+        if(quizArray.includes(item)) {
+          return {value: item, index};
+        }
+      }).filter(Boolean);
+
+      setFindItem(itemData);
+      console.log(itemData);
+      
+      console.log("==========================================================");
     }
-    
-    console.log("==========================================================");
+    catch(error) {
+      console.error(`## insert answer error`);
+    }
   }
 
   const splitString = (inputText: string) => {
@@ -42,11 +65,28 @@ const HomePage = () => {
           <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
       </div>
       <div className=" pt-5 bg-slate-300 w-1/4 border-solid border-2 border-black rounded-md flex justify-between">
-          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
-          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
-          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
-          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
-          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">asd</div>
+        {
+          quizArr.map((item, index) => {
+            console.log("--------------------------->>")
+            let divTag = <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16"></div>; 
+            findItem.map(data => {
+              
+              if(data.index === index) {
+                console.log(`${index} >> data ${data.value}, ${data.index}`);
+                divTag = (<div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16">{data.value}</div>);
+              }
+            })
+            return divTag;
+            console.log("---------------------------<<")
+          })
+        }
+      </div>
+      <div className=" pt-5 bg-slate-300 w-1/4 border-solid border-2 border-black rounded-md flex justify-between">
+      {
+        itemData.map((item, index) => (
+          <div className=" bg-red-300 border-solid border-2 border-black w-1/5 h-16" key={index}>{item}</div>
+        ))
+      }
       </div>
       <div className=" bg-orange-200 h-full w-full">
         <input type="text" onChange={handleForAnwser} onKeyDown={validEnterEvent}></input>
